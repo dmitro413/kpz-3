929b3bc6534e879a1f7fdffb392814b887502953
Дмитро Виноградов <ipz241_vdv@student.ztu.edu.ua>
2026-03-21 18:53:21 +0200

    light command was added to light html

--- ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72ff0df813c13e724c2a531739b33330bf35fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d68847e05426170569609c11eb9904376d0005f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KPZ-LAB3/obj/Debug/net8.0/KPZ-LAB3.AssemblyInfo.cs
+++ KPZ-LAB3/obj/Debug/net8.0/KPZ-LAB3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPZ-LAB3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72ff0df813c13e724c2a531739b33330bf35fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d68847e05426170569609c11eb9904376d0005f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPZ-LAB3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPZ-LAB3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
